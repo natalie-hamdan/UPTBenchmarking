d824e4747dbf00a2b8aeb03bd952f04f2052be85
Natalie Hamdan <nataliehamdan@Natalies-MacBook-Pro.local>
2025-04-29 16:27:02 +0300

    main

---
+++ src/bench/cpu/CPUDigitsOfPi.java
@@ -0,0 +1,30 @@
+package bench.cpu;
+
+import bench.IBenchmark;
+
+public class CPUDigitsOfPi implements IBenchmark {
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void run(Object... params) {
+
+    }
+
+    @Override
+    public void initialize(Object... params) {
+
+    }
+
+    @Override
+    public void clean() {
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
